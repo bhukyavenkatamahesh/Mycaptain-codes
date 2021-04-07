82ff94e9ea3c099d4a4cb6fa1be7970b081d9212
bhukyavenkatamahesh <82050618+bhukyavenkatamahesh@users.noreply.github.com>
2021-04-07 23:54:59 +0530

    Mycaptain code
    C++ program to find the size of int, float, double and char

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int Inttype;
+	float Floattype;
+	char Chartype;
+	double Doubletype;
+	
+	cout<<"size of char  :"<<sizeof(Chartype)<<endl;
+	cout<<"size of int   :"<<sizeof(Inttype)<<endl;
+	cout<<"size of float :"<<sizeof(Floattype)<<endl;
+	cout<<"size of Double:"<<sizeof(Doubletype)<<endl;
+		
+		return 0;
+		
+}
