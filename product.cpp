db7f08f3478c14ce570541cea130e1419b177bdc
bhukyavenkatamahesh <82050618+bhukyavenkatamahesh@users.noreply.github.com>
2021-04-07 23:57:01 +0530

    product of two numbers
    C++ Program to Multiply two Numbers

---
+++ product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double firstNum, secondNum ,TwoNum;
+    cout << "Enter two numbers: ";
+    
+    cin >> firstNum >> secondNum;
+    
+    TwoNumbers = firstNum * secondNum;  
+
+    cout << "Product = " << TwoNumbers;    
+    
+    return 0;
+}
